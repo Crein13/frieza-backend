77ee8138cc1789b9f91bffa2cd1a8382b0751310
ejayazon <eltonjohnyazon@gmail.com>
2025-10-14 02:16:03 +0800

    FRZ-13 + added requiresAuth to the get users API

--- routes/api/v1/user.routes.ts
+++ routes/api/v1/user.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import controller from '@/controllers/v1/users/user.controller.js';
 
+import openid from 'express-openid-connect';
+const { requiresAuth } = openid;
+
 const router = Router();
 
-router.get('/', controller.get);
+router.get('/', requiresAuth(), controller.get);
 
-export const path = '/users';
+export const path = '/api/v1/users';
 export { router };
